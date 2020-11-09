e50e13bcd8d8053e82e8ea521d6ca2af07ee8021
Dimitar Fotev <dimitar.fotev@westernacher.com>
2020-11-09 14:48:16 +0200

    Interpolation search implemented

--- SearchAlgorithms/SearchAlgorithms.js
+++ SearchAlgorithms/SearchAlgorithms.js
@@ -21,4 +21,29 @@ export class SearchAlgorithms {
             return console.log(`Bad input`);
         }
     }
+
+    static interpolationSearch(array, searchedValue) {
+        if (Array.isArray(array) &&
+            typeof (searchedValue) === 'number') {
+
+            let low = 0;
+            let high = array.length - 1;
+
+            let mid = Math.floor(low + ((high - low) / (array[high] - array[low])) * (searchedValue - array[low]));
+
+            if (low > high ||
+                mid > high ||
+                mid < low) {
+                return console.log(`Value ${searchedValue} does not exist in array`);
+            } else if (array[mid] === searchedValue) {
+                return console.log(`Value ${searchedValue} found!`);
+            } else if (array[mid] > searchedValue) {
+                this.interpolationSearch(array.slice(low, mid), searchedValue);
+            } else if (array[mid] < searchedValue) {
+                this.interpolationSearch(array.slice(mid + 1, high + 1), searchedValue);
+            }
+        } else {
+            return console.log(`Bad input`);
+        }
+    }
 }
--- index.js
+++ index.js
@@ -2,15 +2,16 @@ import { SearchAlgorithms } from './SearchAlgorithms/SearchAlgorithms.js'
 
 let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-SearchAlgorithms.binarySearch(array, 1);
-SearchAlgorithms.binarySearch(array, 2);
-SearchAlgorithms.binarySearch(array, 3);
-SearchAlgorithms.binarySearch(array, 4);
-SearchAlgorithms.binarySearch(array, 5);
-SearchAlgorithms.binarySearch(array, 6);
-SearchAlgorithms.binarySearch(array, 7);
-SearchAlgorithms.binarySearch(array, 8);
-SearchAlgorithms.binarySearch(array, 9);
-SearchAlgorithms.binarySearch(array, 10);
-SearchAlgorithms.binarySearch(array, 11);
-SearchAlgorithms.binarySearch(array);
+SearchAlgorithms.interpolationSearch(array, -1);
+SearchAlgorithms.interpolationSearch(array, 1);
+SearchAlgorithms.interpolationSearch(array, 2);
+SearchAlgorithms.interpolationSearch(array, 3);
+SearchAlgorithms.interpolationSearch(array, 4);
+SearchAlgorithms.interpolationSearch(array, 5);
+SearchAlgorithms.interpolationSearch(array, 6);
+SearchAlgorithms.interpolationSearch(array, 7);
+SearchAlgorithms.interpolationSearch(array, 8);
+SearchAlgorithms.interpolationSearch(array, 9);
+SearchAlgorithms.interpolationSearch(array, 10);
+SearchAlgorithms.interpolationSearch(array, 11);
+SearchAlgorithms.interpolationSearch(array);
